65beee9113b9c6daf3fdbedcd185709e2c95fd64
JoaoSilva <jplimadev@gmail.com>
2025-12-19 18:38:54 -0300

    Initial Commit

---
+++ MeuFinalDeSemana/MeuFinalDeSemanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeuFinalDeSemanaApp.swift
+//  MeuFinalDeSemana
+//
+//  Created by Joao Paulo Lima Silva on 19/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MeuFinalDeSemanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
